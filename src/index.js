8904c2565c07ab02a47afe8f5cf734178cb86b1b
Jeremy Lagree <jmlagree@mavs.coloradomesa.edu>
2021-04-11 16:42:11 -0600

    added header info

--- src/index.js
+++ src/index.js
@@ -2,12 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import HeaderInfo from './HeaderInfo'
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
-    <header />
+    <HeaderInfo />
   </React.StrictMode>,
   document.getElementById('root'),
   document.getElementById('header')
