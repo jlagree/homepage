ea6866ccf4208473ccc512971073a043fbcef2f7
Jeremy Lagree <jmlagree@mavs.coloradomesa.edu>
2021-04-11 16:01:11 -0600

    checking status

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> I have oficially ascended.....TWICE.....THRICE...FOUR!
+          Edit <code>src/App.js</code> YEP!
         </p>
         <a
           className="App-link"
