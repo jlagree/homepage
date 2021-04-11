7a43001477d5a8d653529282a0fbaa3e208a0156
Jeremy Lagree <jmlagree@mavs.coloradomesa.edu>
2021-04-11 16:27:29 -0600

    update props

--- src/components/Greeting.js
+++ src/components/Greeting.js
@@ -1,8 +1,7 @@
 import React from 'react';
 
-const Greeting = () => {
-    const name = "Jeremy";
-    return <h1>Hello World! {name} </h1>;
+const Greeting = (props) => {
+    return <h1>Hello World {props.name}! </h1>;
 }
 
 export default Greeting;
