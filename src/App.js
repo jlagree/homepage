a5312319f97c45f71df595fd35eb72bfaa21a1d8
Jeremy Lagree <jmlagree@mavs.coloradomesa.edu>
2021-04-11 17:58:26 -0600

    update nav

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import Header from './components/Header'
 import Footer from './components/Footer'
+import NavBar from './components/NavBar'
 
 function App() {
   return (
     <div className="App">
+      <NavBar />
       <Header />
       <Footer />
     </div>
