dbe97060a11ac5f6649e39f63be9bb8ca9d88479
Jeremy Lagree <jmlagree@mavs.coloradomesa.edu>
2021-04-11 20:54:54 -0600

    Moved back to app.js

--- src/App.js
+++ src/App.js
@@ -2,30 +2,28 @@ import React, { Component } from 'react';
 import {BrowserRouter as Router, Route, Switch, List, Link} from "react-router-dom";
 import Home from './components/Home';
 import About from './components/About';
-import Header from './components/Header';
 
 
 function App() {
   return (
-    // <Router>
-    //     <nav>
-    //       <ul>
-    //         <li>
-    //           <Link to="/">Home</Link>
-    //         </li>
-    //         <li>
-    //           <Link to="/About">About</Link>
-    //         </li>
-    //       </ul>
-    //     </nav>
-    //     <Switch>
-    //       <Route path="/About" component={About}/>
-    //       <Route path="/" exact component={Home}/>
-    //     </Switch>
-    // </Router>
-    <div>
-      <About/>
-    </div>
+    <Router>
+      <div className="container">
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/About">About</Link>
+            </li>
+          </ul>
+        </nav>
+        <Switch>
+          <Route path="/About" component={About}/>
+          <Route path="/" exact component={Home}/>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
