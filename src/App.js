5f8990370fe19c3f0d2fdb64fbb7561e010e8936
Jeremy Lagree <jmlagree@mavs.coloradomesa.edu>
2021-04-11 21:34:41 -0600

    updated header and footer to index.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import {BrowserRouter as Router, Route, Switch, Link} from "react-router-dom";
+import Home from './components/Home';
+import About from './components/About';
 
 function App() {
   const myStyle = {
