bf34885b2e3e86e872428a46d7f42d342c018b34
Jeremy Lagree <jmlagree@mavs.coloradomesa.edu>
2021-04-11 23:50:57 -0600

    new navbar js

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import About from './components/About';
 // import Col from 'react-bootstrap/Col';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
+import BootstrapNavbar from './navbar.js';
 
 function App() {
   const myStyle = {
@@ -15,47 +16,32 @@ function App() {
     width: "100%"
   }
   return (
-    <div>
-      <Navbar bg="dark" variant="dark">
-        <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-        <Nav className="mr-auto">
-        <Nav.Link href="./components/Home.js">Home</Nav.Link>
-        <Nav.Link href="./components/About.js">About</Nav.Link>
-        </Nav>
-      </Navbar>
-      <Router>
-        {/* <Container>
-        <nav>
-              <ul style={myStyle}>
-                <Row>
-                  <Col><li>
-                    <Link to="/">Home</Link>
-                  </li></Col>
-                  <Col><li>
-                    <Link to="/About">About</Link>
-                  </li></Col>
-                </Row>
-              </ul>
-        </nav>
-        </Container> */}
-        <Switch>
-          <Route path="/About" component={About}/>
-          <Route path="/" exact component={Home}/>
-        </Switch>
-      </Router>
-    </div>
+    // <div>
+    //   <Router>
+    //     <Container>
+    //     <nav>
+    //           <ul style={myStyle}>
+    //             <Row>
+    //               <Col><li>
+    //                 <Link to="/">Home</Link>
+    //               </li></Col>
+    //               <Col><li>
+    //                 <Link to="/About">About</Link>
+    //               </li></Col>
+    //             </Row>
+    //           </ul>
+    //     </nav>
+    //     </Container>
+    //     <Switch>
+    //       <Route path="/About" component={About}/>
+    //       <Route path="/" exact component={Home}/>
+    //     </Switch>
+    //   </Router>
+    // </div>
+      <BootstrapNavbar/>
   );
 }
 
 
 export default App;
 
-
-<Navbar bg="dark" variant="dark">
-    <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-    <Nav className="mr-auto">
-      <Nav.Link href="#home">Home</Nav.Link>
-      <Nav.Link href="#features">Features</Nav.Link>
-      <Nav.Link href="#pricing">Pricing</Nav.Link>
-    </Nav>
-</Navbar>
---
+++ src/navbar.js
@@ -0,0 +1,51 @@
+import React from 'react'
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    useParams,
+  } from "react-router-dom";
+  import { Navbar,Nav,NavDropdown,Form,FormControl,Button } from 'react-bootstrap'
+  import Home from './components/Home';
+//   import AboutUs from './components/AboutUs';
+//   import ContactUs from './ContactUs';
+
+class BootstrapNavbar extends React.Component{
+
+    render(){
+        return(
+            <div>
+                <div className="row">
+                    <div className="col-md-12">
+                        <Router>
+                            <Navbar bg="dark" variant="dark" expand="lg" sticky="top">
+                                <Navbar.Brand href="#home">React Bootstrap Navbar</Navbar.Brand>
+                                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                                <Navbar.Collapse id="basic-navbar-nav">
+                                    <Nav className="mr-auto">
+                                    <Nav.Link href="/">Home</Nav.Link>
+                                    <Nav.Link href="/About">About</Nav.Link>
+                                    </Nav>
+                                </Navbar.Collapse>
+                            </Navbar>
+                            <br />
+                            <Switch>
+                                <Route exact path="/">
+                                    <Home />
+                                </Route>
+                                <Route path="/About">
+                                    <About />
+                                </Route>
+                                {/* <Route path="/contact-us">
+                                    <ContactUs />
+                                </Route> */}
+                            </Switch>
+                        </Router>
+                    </div>
+                </div>
+            </div>
+        )  
+    }
+}
+
+export default BootstrapNavbar;
