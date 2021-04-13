adddcb6d91bc9aff29721803b5583fb2e96e8b31
Jeremy Lagree <jmlagree@mavs.coloradomesa.edu>
2021-04-13 16:25:01 -0600

    formatting

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,6 @@ import React from 'react'
 import '../App.css';
 
 const Footer = () => {
-
     return (
         <div className="footer">
             <h3>Created by Jeremy LaGree</h3>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,11 @@
 import React from 'react'
 import headerImage from './spaceHeader.jpeg';
+import '../App.css';
 
 const Header = () => {
-    const imgStyle = {
-        top: "2px",
-        left: "2px",
-        right: "2px",
-        bottom: "2px",
-        width: "100%",
-        height: "10%"
-    }
     return (
         <div className="headerWithImage">
-            <img src={headerImage} style={imgStyle}/> <br/>
+            <img src={headerImage}/> <br/>
         </div>        
     )
 }
