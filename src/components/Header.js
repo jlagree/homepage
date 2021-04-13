7000dd7863084083612bd0f42dd7e995065e3148
Jeremy Lagree <jmlagree@mavs.coloradomesa.edu>
2021-04-13 14:58:54 -0600

    change image format

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,9 +2,15 @@ import React from 'react'
 import headerImage from './spaceHeader.jpeg';
 
 const Header = () => {
+    const imgStyle = {
+        position: "absolute",
+        top: "5px",
+        left: "10px",
+        height: "75px"
+    }
     return (
         <div className="image">
-            <span><img src={headerImage} height="15px" /></span>
+            <img src={headerImage} height="15px" />
             <h1>My Website Header!</h1>
         </div>        
     )
