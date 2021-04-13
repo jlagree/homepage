3216ad1a57598ad14934609e0a7900017d11ccf2
Jeremy Lagree <jmlagree@mavs.coloradomesa.edu>
2021-04-12 18:08:05 -0600

    added header image

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,10 @@
 import React from 'react'
-
+import headerImage from 'spaceHeader';
 
 const Header = () => {
     return (
         <div>
+            <img src={headerImage} height={10} width={100}/>
             <h1>My Website Header!</h1>
         </div>        
     )
