c4dcc855984824c364154012e1f9d69225f0db7c
Jeremy Lagree <jmlagree@mavs.coloradomesa.edu>
2021-04-14 15:35:07 -0600

    change size of website banner text

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -6,7 +6,7 @@ const Banner = () => {
     return (
         <div className="bannerDiv">
             <img src={headerImage} alt="Header-image"/>
-            <div className="imageText">My React Website</div>
+            <div className="imageText"><b>My React Website</b></div>
         </div>       
     )
 }
