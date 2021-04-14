33e0f853a79068187aea9c8785816ae3d3df2a2c
Jeremy Lagree <jmlagree@mavs.coloradomesa.edu>
2021-04-14 11:08:25 -0600

    update style and added certifications paragraphs

--- src/components/Certifications.js
+++ src/components/Certifications.js
@@ -3,7 +3,19 @@ import React from 'react'
 const Certifications = () => {
     return (
         <div>
-            <h1>Certifications</h1>
+            <h1>Certifications/Training Background</h1>
+            <p>I received most of my certifications and training through the military. Before entering the service
+                I had never really been very interested or knowledgeable about computers. The navy really introduced me to technology and from there
+                is where my drive to continue in the field started.</p>
+            <p>I later earned a few more certifications while working at Navakai in Colorado Springs. Upon being hired, we had 90 days
+                to become a MCP(Microsoft Certified Professional). Our company had a partnership with Microsoft, so 70% of our IT staff had to be
+                certified.
+            </p>
+            <p> At Leitner-Poma of America, I have had little time to acquire certifications and instead was at the point where I could really
+                put all my training into practice. Upon moving to the developer role, I started doing most of my training with Udemy courses.
+                These courses tend to focus on web-app development ranging in technologies such as PHP, MySQL, HTML, CSS, JavaScript. I have also
+                began to learn some popular frameworks used for web development.
+            </p>
         </div>
     )
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -23,16 +23,18 @@ const Home = () => {
           <p>
           After leaving the military I was hired at a MSP company in Colorado Springs.
           There, I was a tier II technician where my focus was on Help Desk and server
-          maintenance and administration. I earned my Security plus and windows server 2012 
-          certifications. I later left the company to move back to Grand Jucntion and finish
+          maintenance and administration. While living in Colorado Springs I also attended
+          the University of Colorado, Colorado Springs (UCCS) to pursue a degree in Computer Science. 
+          I later left the company to move back to Grand Jucntion and finish
           my computer science degree at CMU.
           </p>
 
           <p>
-          Currently, I live in Grand Junction (obviously) and work for a company called Leitner-Poma
-          of America. I was just recently moved into the position of web app development there after 
-          previously running the help desk. It has been a transition I looked forward to and it has certianly
-          been a learning experience. I still struggle with many of the technologies, as the sytem has 
+          Currently, I live in Grand Junction and work for a company called Leitner-Poma
+          of America. I was initially hired to do helpdesk and domain administration which is what I did for my first
+          few years of employment here. I was just recently moved into the position of web app development which 
+          has been a transition I looked forward to as my interest in doing IT support has declined over the years. 
+          I still struggle with some of the technologies, as the sytem at LPOA has 
           been in place for quiet some time which makes it all very complex. While it has not been
           extremely inviting to a new developer, I am getting by and hope to continue to grow with the company
           and grow as a developer!
@@ -54,7 +56,7 @@ const Home = () => {
               </tr>
               <tr>
                 <td>2018-2020</td>
-                <td>Helpdesk/Network Adminitrator, Leitner-Poma of America</td>
+                <td>Helpdesk/Network Administrator, Leitner-Poma of America</td>
               </tr>
               <tr>
                 <td>2018-Present</td>
@@ -62,7 +64,7 @@ const Home = () => {
               </tr>
               <tr>
                 <td>2016-2018</td>
-                <td>Tier II Network Adminitrator, Navakai Inc.</td>
+                <td>Tier II Network Administrator, Navakai Inc.</td>
               </tr>
               <tr>
                 <td>2016-2018</td>
